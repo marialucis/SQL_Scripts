96168cea05be28836a38f8795c0fdf197b8a90b8
Luci Sousa <marialuci148@gmail.com>
2022-02-01 16:52:03 -0300

    subindo novos arquivos oracle

--- ORACLE/SCRIPTS_CONSULTAS/Criacao_Usuario_Permissao.sql
+++ ORACLE/SCRIPTS_CONSULTAS/Criacao_Usuario_Permissao.sql
@@ -4,7 +4,7 @@ CREATE USER teste_homologacao IDENTIFIED BY homo123 DEFAULT TABLESPACE users;
 
 --Crinado permissao SUPER
 GRANT ALL PRIVILEGIES TO teste_homologacao;
-
+ALTER USER teste_homologacao QUOTA UNLIMITED ON USERS;
 --Criando permissao/privilegios simples ao usuario
 GRANT CREATE SESSION TO teste_homologacao;
 
