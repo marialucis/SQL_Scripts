983e34b61bfcfe84b445e795c0195046210b3d5d
Luci Sousa <marialuci148@gmail.com>
2022-04-21 18:39:30 -0300

    Update Criacao_Usuario_Permissao.sql

--- ORACLE/SCRIPTS/COMANDOS DCL/Criacao_Usuario_Permissao.sql
+++ ORACLE/SCRIPTS/COMANDOS DCL/Criacao_Usuario_Permissao.sql
@@ -3,7 +3,7 @@ alter session set "_oracle_script"=true;
 CREATE USER teste_homologacao IDENTIFIED BY homo123 DEFAULT TABLESPACE users;
 
 --Crinado permissao SUPER
-GRANT ALL PRIVILEGIES TO teste_homologacao;
+GRANT ALL PRIVILEGES TO teste_homologacao;
 ALTER USER teste_homologacao QUOTA UNLIMITED ON USERS;
 --Criando permissao/privilegios simples ao usuario
 GRANT CREATE SESSION TO teste_homologacao;
